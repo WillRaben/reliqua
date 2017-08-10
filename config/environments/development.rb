9f22fd3031d4b09840a8d38712d722e36d6dcef2
Will Raben <wilmer.raben@gmail.com>
2017-08-10 08:39:45 -0600

    fixed Cannot render console from 192.168.11.1 error by adding it to development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  #Added by will
+  class Application < Rails::Application
+  config.web_console.whitelisted_ips = '192.168.11.1'
+  end
+
+
+
 end
