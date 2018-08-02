b7b23e6cb3d385a547d1b9cc5d50897fa826063a
Will Raben <wilmer.raben@gmail.com>
2018-08-01 21:22:58 -0600

    SSL Added webpack exposed on https

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-Puma: rails s -b 0.0.0.0
+Puma: rails s -b 'ssl://0.0.0.0:5000?key=/home/will/dev/ssl/localhost.key&cert=/home/will/dev/ssl/localhost.crt'
 Webpack-dev: ./bin/webpack-dev-server --host 192.168.11.129 --port 8080 --content-base
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  scope "/admin" do
+  	resources :users
+	end
   #root to: home#index
 end
